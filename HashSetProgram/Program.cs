a1c1978decdc872d5beea84a5debf46aae47b7cf
Nishandhini Amulraj <nishandhini_amulraj@epam.com>
2025-07-11 09:09:57 +0530

    Add project files.

---
+++ HashSetProgram/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashSetProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> employees = new HashSet<string>();
+            employees.Add("Abi");
+            employees.Add("Catherine");
+            employees.Add("Shiny");
+            employees.Add("Abi");
+
+            Console.WriteLine($"Count after adding employees: {employees.Count}"); // Outputs 3
+
+            // Check for existence
+            if (employees.Contains("Shiny"))
+            {
+                Console.WriteLine("Shiny is in the set.");
+            }
+
+            // Remove an element
+            employees.Remove("Shiny");
+            Console.WriteLine($"Count after removal: {employees.Count}"); // Outputs 2
+
+            // Iterate through the set
+            Console.WriteLine("List Employees in the set:");
+            foreach (var e in employees)
+            {
+                Console.WriteLine($"- {e}");
+            }
+
+        }
+    }
+}
